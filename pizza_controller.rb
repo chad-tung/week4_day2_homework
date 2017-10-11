471f86d713487dc1b68ec9802d78d8337d88cf92
chad-tung <chad.tung@gmail.com>
2017-10-11 01:15:17 +0100

    Pizza stuff

---
+++ pizza_controller.rb
@@ -0,0 +1,48 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative './models/pizza'
+
+get '/pizzas' do
+    @pizzas = Pizza.all()
+    erb( :index )
+end
+
+get '/pizzas/new' do
+    erb( :new )
+end
+
+post '/pizzas/:id/delete' do
+    @pizza = Pizza.find(params['id'])
+    @pizza.delete()
+    erb( :cancelled )
+end
+
+# get '/pizzas/:id/cancel' do
+#     @pizza = Pizza.find(params['id'])
+#     @pizza.delete()
+#     erb( :cancelled )
+# end
+
+get '/pizzas/:id/edit' do
+    @pizza = Pizza.find(params['id'])
+    erb( :edit )
+end
+
+get '/pizzas/:id' do
+    @pizza = Pizza.find(params['id'])
+    erb( :show )
+end
+
+post '/pizzas/:id' do
+    @pizza = Pizza.new(params)
+    @pizza.update()
+    erb( :updated )
+end
+
+post '/pizzas' do
+    @pizza = Pizza.new(params)
+    @pizza.save()
+    erb( :create )
+end
